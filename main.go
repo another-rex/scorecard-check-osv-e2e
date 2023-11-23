ba0c313a3ddbe27e302e626f5a1aa282b8e8bc00
Rex P <rexpan@google.com>
2023-11-24 11:23:08 +1300

    Import vulnerable package

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/gogo/protobuf/version"
+import (
+	"log"
+
+	"github.com/gogo/protobuf/plugin/unmarshal"
+	"github.com/gogo/protobuf/version"
+)
 
 func main() {
+	marshaler := unmarshal.NewUnmarshal()
 	print(version.AtLeast("v1.2.3"))
+	log.Printf("%v", marshaler)
 }
